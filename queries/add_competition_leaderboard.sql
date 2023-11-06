4155e754be0d927a8b737c51647a6947bab3243f
Manitary <pak.ska@gmail.com>
2023-11-06 00:20:51 +0000

    Add queries to update the db with the new command

---
+++ queries/add_competition_leaderboard.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "competition_scores" (
+	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"user_id" INTEGER NOT NULL,
+	"competition_pokemon" TEXT NOT NULL,
+	"score" INTEGER NOT NULL,
+	"message_id" INTEGER NOT NULL UNIQUE,
+	"message_url" TEXT NOT NULL UNIQUE,
+	"image_url" TEXT NOT NULL,
+	"date" TEXT NOT NULL,
+	"verified" INTEGER NOT NULL DEFAULT 0
+)
+
+INSERT INTO commands
+    ("command_name", "module_name", "method_name", "command_type", "command_tier", "description")
+VALUES
+    ("leaderboard", "shuffle_commands", "competition_leaderboard", "prefix", 1, "Display the leaderboard for a competitive stage")
+;
+
+INSERT INTO settings
+    ("key", "value")
+VALUES
+    ("comp_leaderboard_size_max", "10"),
+    ("comp_leaderboard_size_default", "5"),
+    ("message_leaderboard_no_param", "I need a Competitive Stage Pokemon"),
+    ("message_leaderboard_no_result", "That Pokemon doesn’t seem to have a Competitive Stage")
+;
