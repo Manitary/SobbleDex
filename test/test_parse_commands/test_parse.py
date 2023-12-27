3cf0e71d671c01367d678ce4dc60a4d92e14817b
Manitary <pak.ska@gmail.com>
2023-12-27 23:36:35 +0000

    Add examples of invalid queries or using `"`

--- test/test_parse_commands/test_parse.py
+++ test/test_parse_commands/test_parse.py
@@ -13,7 +13,7 @@ type Context_Payloads = tuple[KoduckContext, list[Payload], list[CtxData]]
 SAMPLES_PATH = Path(__file__).resolve().parent.parent / "assets" / "user_queries.json"
 
 with SAMPLES_PATH.open(encoding="utf-8") as f:
-    QUERIES_SAMPLES = json.load(f)
+    QUERIES_SAMPLES: list[tuple[str, CtxData]] = json.load(f)
 
 
 @pytest.mark.parametrize("user_msg, expected", QUERIES_SAMPLES)
@@ -27,3 +27,16 @@ async def test_on_message(
     await on_message(context.message)
 
     assert context_data[-1] == tuple(expected)
+
+
+@pytest.mark.parametrize("user_msg", ("?test", '?"q" mega', '"?q mega"', " ?stage 1"))
+@pytest.mark.asyncio
+async def test_on_message_invalid(
+    context_archive: AsyncIterator[Context_Payloads], user_msg: str
+) -> None:
+    context, _, context_data = await anext(context_archive)
+    assert context.message
+    context.message.content = user_msg
+    await on_message(context.message)
+
+    assert not context_data
